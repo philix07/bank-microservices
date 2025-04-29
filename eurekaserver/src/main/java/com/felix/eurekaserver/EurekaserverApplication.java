e7d4cf7a111c92b6811441770e27ce59ee0b60cd
Felix Liando <fliando1023@gmail.com>
2025-04-29 17:06:24 +0700

    Modified Eureka Server properties

--- eurekaserver/src/main/java/com/felix/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/felix/eurekaserver/EurekaserverApplication.java
@@ -2,12 +2,16 @@ package com.felix.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// Using this annotation we're converting from a normal springboot project
+// to act as a Service Discovery agent with the help of Eureka library
+@EnableEurekaServer
 public class EurekaserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaserverApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaserverApplication.class, args);
+  }
 
 }
