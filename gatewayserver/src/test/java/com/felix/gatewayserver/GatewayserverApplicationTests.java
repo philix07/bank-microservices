31ffb033558f2894dd97c695ff0614602498e0f8
Felix Liando <fliando1023@gmail.com>
2025-04-30 14:57:14 +0700

    Initialized gatewayserver for creating API Gateway (blank project)

---
+++ gatewayserver/src/test/java/com/felix/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
