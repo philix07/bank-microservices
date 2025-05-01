8b7cf6d43d0b6fb958ad3d8009c5744528bbbb56
Felix Liando <fliando1023@gmail.com>
2025-05-01 15:42:31 +0700

    Created custom RouteLocator for api gateway

--- gatewayserver/src/main/java/com/felix/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/felix/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,34 @@ package com.felix.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayserverApplication.class, args);
+  }
+
+  @Bean
+  RouteLocator miniBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+    return routeLocatorBuilder.routes()
+      .route(p -> p.
+        path("/minibank/accounts/**")
+        .filters(f -> f.rewritePath("/minibank/accounts/(?<segment>.*)", "/${segment}"))
+        .uri("lb://ACCOUNTS"))
+      .route(p -> p.
+        path("/minibank/cards/**")
+        .filters(f -> f.rewritePath("/minibank/cards/(?<segment>.*)", "/${segment}"))
+        .uri("lb://CARDS"))
+      .route(p -> p.
+        path("/minibank/loans/**")
+        .filters(f -> f.rewritePath("/minibank/loans/(?<segment>.*)", "/${segment}"))
+        .uri("lb://LOANS"))
+      .build();
+
+  }
 
 }
