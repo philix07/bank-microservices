810f8e6a9e52ea1e67882db26ff954c6f192ffb6
Felix Liando <fliando1023@gmail.com>
2025-05-13 16:24:01 +0700

    Modified Gateway Server

--- gatewayserver/src/main/java/com/felix/gatewayserver/filters/RequestTraceFilter.java
+++ gatewayserver/src/main/java/com/felix/gatewayserver/filters/RequestTraceFilter.java
@@ -1,6 +1,47 @@
 package com.felix.gatewayserver.filters;
 
-public class RequestTraceFilters {
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+// @Order(n) means execution order of the filters, it starts from 1, 2, 3, ...
+// @Order(1) make sure this filter is executed first inside the gateway server
+@Order(1)
+@Component
+public class RequestTraceFilter implements GlobalFilter {
 
+  private static final Logger logger = LoggerFactory.getLogger(RequestTraceFilter.class);
+
+  @Autowired
+  FilterUtility filterUtility;
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+    if (isCorrelationIdPresent(requestHeaders)) {
+      logger.debug("minibank-correlation-id found in RequestTraceFilter : {}",
+        filterUtility.getCorrelationId(requestHeaders));
+    } else {
+      String correlationID = generateCorrelationId();
+      exchange = filterUtility.setCorrelationId(exchange, correlationID);
+      logger.debug("minibank-correlation-id generated in RequestTraceFilter : {}", correlationID);
+    }
+    return chain.filter(exchange);
+  }
+
+  private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+    return filterUtility.getCorrelationId(requestHeaders) != null;
+  }
+
+  private String generateCorrelationId() {
+    return java.util.UUID.randomUUID().toString();
+  }
 }
