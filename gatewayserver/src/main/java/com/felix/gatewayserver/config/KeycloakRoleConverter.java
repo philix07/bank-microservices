6b74c3a9f961eabd71a03308cac9cbdeb905fbb0
Felix Liando <fliando1023@gmail.com>
2025-06-02 12:09:42 +0700

    Finished Authentication and Authorization configuration using Keycloak

--- gatewayserver/src/main/java/com/felix/gatewayserver/config/KeycloakRoleConverter.java
+++ gatewayserver/src/main/java/com/felix/gatewayserver/config/KeycloakRoleConverter.java
@@ -1,4 +1,36 @@
 package com.felix.gatewayserver.config;
 
-public class KeycloakRoleConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+  @Override
+  public Collection<GrantedAuthority> convert(Jwt source) {
+    // For more details about the payload format, convert the JWT token in 'https://jwt.io/'
+    // The 'getClaims()' method lets you access the payload (claims) inside the access token.
+    // 'get("realm_access")' retrieves the data associated with the key "realm_access" in the payload.
+    // The "realm_access" section contains all the roles assigned to the current client.
+    Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+    if (realmAccess == null || realmAccess.isEmpty()) {
+      return new ArrayList<>();
+    }
+
+    Collection<GrantedAuthority> returnValue = ((List<String>) realmAccess.get("roles"))
+      // We require the "ROLE_" prefix because Spring Security is using that format
+      .stream().map(roleName -> "ROLE_" + roleName)
+      .map(SimpleGrantedAuthority::new)
+      .collect(Collectors.toList());
+    return returnValue;
+
+  }
+
 }
--- gatewayserver/src/main/java/com/felix/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/felix/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,17 @@ package com.felix.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,26 +25,41 @@ public class SecurityConfig {
 //      .anyExchange().authenticated()
 //    );
 
+    // 'exchanges' refers to incoming HTTP requests.
+    // The order of configuration determines priority (top to bottom: 1 > 2 > 3).
+    // In the configuration below:
+    // - All GET requests are allowed without authentication.
+    //   It means you can still fetch minibank/accounts, but you can't do POST or UPDATE or DELETE
+    // - Any request to '/minibank/accounts/**', '/minibank/cards/**', or '/minibank/loans/**'
+    //   must be authenticated.
     return serverHttpSecurity
-      // 'exchanges' refers to incoming HTTP requests.
-      // The order of configuration determines priority (top to bottom: 1 > 2 > 3).
-      // In the configuration below:
-      // - All GET requests are allowed without authentication.
-      //   It means you can still fetch minibank/accounts, but you can't do POST or UPDATE or DELETE
-      // - Any request to '/minibank/accounts/**', '/minibank/cards/**', or '/minibank/loans/**'
-      //   must be authenticated.
       .authorizeExchange(exchanges -> exchanges
         .pathMatchers(HttpMethod.GET).permitAll()
-        .pathMatchers("/minibank/accounts/**").authenticated()
-        .pathMatchers("/minibank/cards/**").authenticated()
-        .pathMatchers("/minibank/loans/**").authenticated()
+        .pathMatchers("/minibank/accounts/**").hasRole("ACCOUNTS")
+        .pathMatchers("/minibank/cards/**").hasRole("CARDS")
+        .pathMatchers("/minibank/loans/**").hasRole("LOANS")
       )
       .oauth2ResourceServer(resourceServer -> resourceServer
-        .jwt(Customizer.withDefaults())
+        // grantedAuthoritiesExtractor() is a custom converter to extract granted authorities (roles) from a JWT token.
+        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor()))
       )
       .csrf(csrfSpec -> csrfSpec.disable())
       .build();
+  }
 
+  // This method creates a custom converter to extract granted authorities (roles) from a JWT token.
+  private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+
+    // Create a standard JWT to AuthenticationToken converter provided by Spring Security.
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+
+    // Customize the converter by setting a Keycloak-specific converter that extracts roles from the token.
+    // KeycloakRoleConverter is a custom class that transforms Keycloak-specific role claims into Spring Security authorities.
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+    // Wrap the converter with a Reactive adapter since we're using reactive security (WebFlux).
+    // This makes it compatible with reactive applications and returns a Mono (asynchronous container).
+    return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
   }
 
 }
